b196e01c3b4f56dc72e9ef3127bd09ecf39fa235
User <User@N13090.intellectdesign.com>
2021-09-23 11:42:21 +0530

    second project

---
+++ Mygithub/src/com/repoo/ClassHello.java
@@ -0,0 +1,11 @@
+package com.repoo;
+
+public class ClassHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World all");
+
+	}
+
+}
